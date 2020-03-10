cfadb9feeb27a021eecf4dc3cc74bef77f8704bb
blacktunes <1719513828@qq.com>
2020-03-10 11:10:10 +0800

    demo完成

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,65 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <input type="text" v-model="text" maxlength="100">
+    <button @click="click">提交</button>
+    <span>字数：{{text.length}}/100</span>
+    <p>（最少五个字）</p>
+    <hr>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li v-for="(item, index) in list" :key="index">{{item.text}}</li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <hr>
+    <input type="text" v-model="text" maxlength="100">
+    <button @click="click">提交</button>
+    <span>字数：{{text.length}}/100</span>
+    <p>（最少五个字）</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      text: '',
+      list: []
+    }
+  },
+  methods: {
+    click () {
+      if (this.text.length < 5) {
+        return
+      }
+      axios({
+        url: 'https://www.feizhouxianyu.cn:9090/mengxinjuzhusuo',
+        method: 'post',
+        params: {
+          text: this.text
+        }
+      }).then(res => {
+        if (res.status === 200) {
+          this._getList()
+          this.text = ''
+        }
+    })
+    },
+    _getList () {
+      axios.get('https://www.feizhouxianyu.cn:9090/mengxinjuzhusuo').then(res => {
+        if (res.status === 200) {
+          this.list = res.data.results
+        }
+      })
+    }
+  },
+  created () {
+    this._getList()
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
