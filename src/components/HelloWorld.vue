b429d49768aeb9adbd7316d8dcd91c98cf695457
blacktunes <1719513828@qq.com>
2020-03-10 14:20:02 +0800

    消息发送方式改为WS、添加位置跳转、添加在线人数显示

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="hello">
-    <input type="text" v-model="text" maxlength="100">
-    <button @click="click">提交</button>
-    <span>字数：{{text.length}}/100</span>
-    <p>（最少五个字）</p>
+  <div id="mengxin">
+    <div class="menu">
+      <div @click="isToBottom">
+        <input type="checkbox" v-model="toBottom">
+        <span>有新消息自动跳转最后</span>
+      </div>
+      <button @click="scrollToBottom">跳到最底</button>
+    </div>
     <hr>
-    <ul>
+    <p>在线人数：{{onlineNum}}</p>
+    <hr>
+    <input type="text" v-model="text" maxlength="100" placeholder="最少五个字">
+    <button @click="send">提交</button>
+    <span>字数：{{text.length}}/100</span>
+    <hr>
+    <ol>
       <li v-for="(item, index) in list" :key="index">{{item.text}}</li>
-    </ul>
+    </ol>
     <hr>
-    <input type="text" v-model="text" maxlength="100">
-    <button @click="click">提交</button>
+    <input type="text" v-model="text" maxlength="100" placeholder="最少五个字">
+    <button @click="send">提交</button>
     <span>字数：{{text.length}}/100</span>
-    <p>（最少五个字）</p>
+    <hr>
+      <p>在线人数：{{onlineNum}}</p>
+    <hr>
+    <div class="menu">
+      <div @click="isToBottom">
+        <input type="checkbox" v-model="toBottom">
+        <span>有新消息自动跳转最后</span>
+      </div>
+      <button @click="scrollToTop">回到顶部</button>
+    </div>
+    
   </div>
 </template>
 
@@ -20,31 +39,82 @@
 import axios from 'axios'
 
 export default {
-  name: 'HelloWorld',
   data () {
     return {
       text: '',
-      list: []
+      list: [],
+      toBottom: false,
+      onlineNum: 0
+    }
+  },
+    sockets: {
+    // 建立连接自动调用connect
+    connect: function () {
+      // 与socket.io连接后回调
+      // console.log('conn')
+    },
+    connect_error: () => {
+      // console.log('connErr')
+    },
+    disconnect: () => {
+      // console.log('disconn')
+    },
+    receiveMessage: function (data) {
+      this.list.push(data)
+      this.scrollToBottom()
+    },
+    updateOnline: function (num) {
+      this.onlineNum = num
     }
   },
   methods: {
-    click () {
+    isToBottom () {
+      this.toBottom = !this.toBottom
+    },
+    send () {
       if (this.text.length < 5) {
         return
       }
-      axios({
-        url: 'https://www.feizhouxianyu.cn:9090/mengxinjuzhusuo',
-        method: 'post',
-        params: {
-          text: this.text
-        }
-      }).then(res => {
-        if (res.status === 200) {
-          this._getList()
-          this.text = ''
-        }
-    })
+      this.$socket.emit('sendMessage', {
+        text: this.text,
+      })
+      this.text = ''
     },
+    autoTOBottom () {
+      if (this.toBottom) {
+        this.scrollToBottom()
+      }
+    },
+    scrollToBottom () {
+      this.$nextTick(() => {
+        document.body.scrollTop = document.body.scrollHeight
+        document.documentElement.scrollTop = document.documentElement.scrollHeight
+        // let speed = document.documentElement.scrollHeight / 10
+        // var timer = setInterval(() => {
+        //   document.documentElement.scrollTop += speed
+        //   console.log(document.documentElement.scrollTop + document.documentElement.clientHeight, document.documentElement.scrollHeight)
+        //   if (document.documentElement.scrollTop + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        //     clearInterval(timer)
+        //     timer = null
+        //   }
+        // }, 30)
+      })
+     },
+     scrollToTop () {
+       this.$nextTick(() => {
+         document.body.scrollTop = 0
+         document.documentElement.scrollTop = 0
+        // let speed = document.documentElement.scrollHeight / 10
+        // var timer = setInterval(() => {
+        //   console.log(timer)
+        //   document.documentElement.scrollTop -= speed
+        //   if (document.documentElement.scrollTop === 0) {
+        //     clearInterval(timer)
+        //     timer = null
+        //   }
+        // }, 30)
+      })
+     },
     _getList () {
       axios.get('https://www.feizhouxianyu.cn:9090/mengxinjuzhusuo').then(res => {
         if (res.status === 200) {
@@ -61,5 +131,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.menu {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSocketIO from 'vue-socket.io'
 
 Vue.config.productionTip = false
+
+Vue.use(new VueSocketIO({
+  debug: false,
+  connection: 'https://www.feizhouxianyu.cn:9090'
+}))
 
 new Vue({
   render: h => h(App),
